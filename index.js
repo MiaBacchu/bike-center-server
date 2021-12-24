af026328733e6c281e9e95201937adc3f734dee0
Mia Bacchu <jewelranabacchu@gmail.com>
2021-12-24 12:34:29 +0600

    access allowed

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const port=process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
-    res.header({"Access-Control-Allow-Origin": "*"});
-    next();
+    res.header("Access-Control-Allow-Origin", "*")
   })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m0n59.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
